fdf39d91ccd405d72b9bd0291cb2b327b7f5a785
Karim Mohamed <37942217+KarimMohamed2005@users.noreply.github.com>
2020-03-16 19:53:12 +0200

    Update dbConnection.js

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 module.exports = () => {
   return mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'contraseña',
+    user: 'lero',
+    password: 'lero',
     database: 'news_portal'
   });
 }
